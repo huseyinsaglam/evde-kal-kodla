969a5105152d2c582d3e4aca09d9567808195c4a
Huseyin Saglam <hsaglam001@gmail.com>
2020-04-06 16:59:18 +0300

    evde-kal-kodla

---
+++ evde-kal-kodla/src/main/java/com/application/evdekalkodla/EvdeKalKodlaApplication.java
@@ -0,0 +1,13 @@
+package com.application.evdekalkodla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvdeKalKodlaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvdeKalKodlaApplication.class, args);
+	}
+
+}
---
+++ evde-kal-kodla/src/test/java/com/application/evdekalkodla/EvdeKalKodlaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.evdekalkodla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvdeKalKodlaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
