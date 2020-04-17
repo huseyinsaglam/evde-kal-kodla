bc87f5c87e200919606c8296ea0f09803a4f4e37
Huseyin Saglam <hsaglam001@gmail.com>
2020-04-17 14:56:38 +0300

    evde-kal-kodla-gui

---
+++ evde-kal-kodla/src/webui/src/app/pages/subject/subject.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SubjectComponent } from './subject.component';
+
+
+
+@NgModule({
+  declarations: [SubjectComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class SubjectModule { }
