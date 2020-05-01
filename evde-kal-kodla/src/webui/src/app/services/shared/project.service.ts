64e7f5490bd10658ae71ee2df5e233a792bbb1b9
Huseyin Saglam <hsaglam001@gmail.com>
2020-05-01 17:38:21 +0300

    evde-kal-kodla-service

--- evde-kal-kodla/src/webui/src/app/services/shared/project.service.ts
+++ evde-kal-kodla/src/webui/src/app/services/shared/project.service.ts
@@ -2,14 +2,17 @@ import {Injectable} from "@angular/core";
 import {ApiService} from "../api.service";
 import {Observable} from "rxjs";
 import {map} from "rxjs/operators";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {environment} from "../../../environments/environment";
 
 @Injectable()
 export class ProjectService {
 
-  constructor(private apiService : ApiService)
+  constructor(private apiService : ApiService,private http: HttpClient)
   {
 
   }
+
 
   getAll(page):Observable<any>
   {
@@ -29,7 +32,7 @@ export class ProjectService {
       ));
   }
 
-  // http://localhost:8080/project/2
+
   getByid(id):Observable<any>
   {
     return this.apiService.get("/project",id).pipe(map(
@@ -48,6 +51,7 @@ export class ProjectService {
       }
     ));
   }
+
 
 
   createProject(project):Observable<any>
@@ -70,7 +74,7 @@ export class ProjectService {
   }
 
 
-  // http://localhost:8080/project&id=3   (id=3) HttpParams ile aldi
+
   delete(id):Observable<any>
   {
     return this.apiService.delete("/project",id).pipe(map(
@@ -89,6 +93,124 @@ export class ProjectService {
       }
     ));
   }
+
+
+
+
+/*
+
+
+
+  // Http Options
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  }
+
+  // HttpClient API get() method => Fetch project list
+  getAll(page): Observable<any> {
+    return this.http.get(environment.API_BASE_PATH + '/project/pagination?page=' + page)
+      .pipe(map(
+        response =>
+        {
+          if (response != null)
+          {
+            return response
+          }
+          else
+          {
+            console.log(response);
+            return {};
+          }
+        }
+      ));
+  }
+
+  // HttpClient API get() method => Fetch project
+  getByid(id): Observable<any> {
+    return this.http.get(environment.API_BASE_PATH  + '/project/' + id)
+      .pipe(map(
+        response =>
+        {
+          if (response != null)
+          {
+            return response
+          }
+          else
+          {
+            console.log(response);
+            return {};
+
+          }
+        }
+      ));
+  }
+
+  // HttpClient API post() method => Create project
+  createProject(project): Observable<any> {
+    return this.http.post(environment.API_BASE_PATH  + '/project', JSON.stringify(project), this.httpOptions)
+      .pipe(map(
+        response =>
+        {
+          if (response != null)
+          {
+            return response
+          }
+          else
+          {
+            console.log(response);
+            return {};
+
+          }
+        }
+      ))
+  }
+
+  // HttpClient API put() method => Update project
+  updateProject(id, project): Observable<any> {
+    return this.http.put(environment.API_BASE_PATH  + '/project/' + id, JSON.stringify(project), this.httpOptions)
+      .pipe(map(
+        response =>
+        {
+          if (response != null)
+          {
+            return response
+          }
+          else
+          {
+            console.log(response);
+            return {};
+
+          }
+        }
+      ));
+  }
+
+  // HttpClient API delete() method => Delete project
+  delete(id){
+    return this.http.delete(environment.API_BASE_PATH  + '/project/' + id, this.httpOptions)
+      .pipe(map(
+        response =>
+        {
+          if (response != null)
+          {
+            return response
+          }
+          else
+          {
+            console.log(response);
+            return {};
+
+          }
+        }
+      ));
+  }
+
+
+
+*/
+
 
 
 }
